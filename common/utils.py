a5fbce3a5436df021da3e70c2b3c1df1b0b25abc
powfu <3220475550@qq.com>
2025-04-07 17:31:48 +0800

    图片验证码生成及前端控制器开发

---
+++ common/utils.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import random
+import string
+from io import BytesIO
+
+# PIL就是pillow
+from PIL import Image, ImageFont
+from PIL import ImageDraw
+
+
+class ImageCode():
+    def rand_color(self):
+        red = random.randint(0, 255)
+        green = random.randint(0, 255)
+        blue = random.randint(0, 255)
+        return red, green, blue
+
+    def draw_lines(self,draw,num,width,height):
+        for i in range(num):
+            x1 = random.randint(8,width-1)
+            y1 = random.randint(8,height-1)
+            x2 = random.randint(8,width-1)
+            y2 = random.randint(8,height-1)
+            draw.line(((x1,y1),(x2,y2)), fill=self.rand_color(), width=random.randint(1,5))
+
+    def get_text(self):
+        list = random.sample(string.ascii_letters+string.digits,4)
+        return ''.join(list)
+
+    def draw_verify_code(self):
+        # 生成随机字符串
+        code = self.get_text()
+        print(code)
+        width,height = 120,50
+        image = Image.new('RGB',(width,height),"white")
+        font = ImageFont.truetype('font/FREESCPT.TTF', 50)
+        draw = ImageDraw.Draw(image)
+        # 绘制字符串
+        for i in range(4):
+            draw.text((random.randint(3,10)+25*i,random.randint(1,2)), text=code[i], font=font, fill=self.rand_color())
+
+        # 绘制干扰线
+        self.draw_lines(draw,4,width,height)
+
+        # image.show()
+        return image,code
+
+    def get_code(self):
+        image,code = self.draw_verify_code()
+
+        # 创建一个内存二进制流对象（类似临时文件）
+        buf = BytesIO()
+
+        # 将 PIL 图像保存到二进制流中，格式为 JPEG
+        image.save(buf, format='jpeg')
+
+        # 从二进制流中获取字节数据
+        image_b_string = buf.getvalue()
+
+        return code,image_b_string
+
+ic = ImageCode()
+ic.draw_verify_code()
--- controller/user.py
+++ controller/user.py
@@ -1,12 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, session
 
-from model.user import User
+from common.utils import ImageCode
+
+# from model.user import User
 
 user = Blueprint('user', __name__)
 
-@user.route('/user')
-def get_one():
-    user = User()
-    result = user.get_one()
-    print (result.__dict__)
-    return "ok"
+# @user.route('/user')
+# def get_one():
+#     user = User()
+#     result = user.get_one()
+#     print (result.__dict__)
+#     return "ok"
+
+@user.route('/vcode')
+def vcode():
+    # 调用 ImageCode 类的 get_code 方法，获取验证码文本和图像二进制数据
+    code,bstring = ImageCode().get_code()
+
+    # 将图像二进制数据包装成 Flask 响应对象
+    response = make_response(bstring)
+
+    # 设置响应头，告诉浏览器这是 JPEG 图片
+    response.headers['Content-Type'] = 'image/jpeg'
+
+    # 将验证码文本转为小写后存入 session（用于后续验证）
+    session['vcode'] = code.lower()
+
+    # 返回包含验证码图片的响应
+    return response
