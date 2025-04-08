6f9dc68b050c945ca9506b63d2dbe452999889ff
powfu <3220475550@qq.com>
2025-04-08 10:51:27 +0800

    front-end reg & lil fix for res_msg

--- common/response_message.py
+++ common/response_message.py
@@ -1,10 +1,10 @@
 class UserMessage():
     @staticmethod
     def success(data):
-        return {"statue0":1000,"data":data}
+        return {"status":1000,"data":data}
     @staticmethod
     def other(data):
-        return {"statue0":1001,"data":data}
+        return {"status":1001,"data":data}
     @staticmethod
     def error(data):
-        return {"statue0":1002,"data":data}
+        return {"status":1002,"data":data}
